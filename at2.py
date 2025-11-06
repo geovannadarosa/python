8cfec886957ae397c0a88bfbb262ba2f218c5c87
geovannadarosa <geovannadarosava@gmail.com>
2025-11-06 16:20:55 -0400

    criei um programa que mostra a quantidade de habitantes em algumas cidades

---
+++ at2.py
@@ -0,0 +1,15 @@
+cidade = input('digite o nome da cidade:')
+if cidade == 'Campo Grande':
+    print('800 mil habitantes')
+if cidade == 'Anhanduí':
+    print('5 mil habitantes')
+if cidade == 'Terenos':
+    print('10 mil habitantes')
+if cidade == 'Sodrolândia':
+    print('50 mil habitantes')
+if cidade == 'Bonito':
+    print('100 mil habitantes')
+if cidade == 'Costa Rica':
+    print('45 mil habitantes')
+if cidade == 'Miranda':
+    print('35 mil habitantes')
