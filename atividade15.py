dfee8e5de345820e9e51ba1c0123da9cf3c17928
geovannadarosa <geovannadarosava@gmail.com>
2025-11-28 15:37:30 -0400

    Add files via upload

---
+++ atividade15.py
@@ -0,0 +1,7 @@
+genero = input('digite o seu genero (F - feminino M - Masculino):')
+if genero  == 'F':
+    print('feminino')
+elif genero == 'M':
+    print('masculino')
+else:
+    print('Genero invalido')
