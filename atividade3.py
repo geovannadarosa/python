75a44390f3443f6cdf914ca5831e9d69a42585a0
geovannadarosa <geovannadarosava@gmail.com>
2025-11-28 15:59:57 -0400

    Add files via upload

---
+++ atividade3.py
@@ -0,0 +1,4 @@
+nome = input('digite seu nome:')
+titulo = int(input('digite o seu titulo eleitoral:'))
+candidato = int(input('digite o numero do seu candidato:'))
+print(f'o seu nome:{nome} \no seu titulo eleitoral:{titulo} \no numero do seu candidato:{candidato}')
