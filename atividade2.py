2190c71ec3c4e458d481194482ac210a05bdfe25
geovannadarosa <geovannadarosava@gmail.com>
2025-11-28 15:53:34 -0400

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,8 @@
+nome = input('digite o nome do aluno:')
+disciplina = input('digite o nome da sua disciplina:')
+nota1 = float(input('digite sua nota do primeiro bimestre:'))
+nota2 = float(input('digite sua nota do segundo bimestre:'))
+nota3 = float(input('digite sua nota do terceiro bimestre:'))
+nota4 = float(input('digite sua nota do quarto bimestre:'))
+
+print(f"aluno {nome} \na disciplina da suas notas são {disciplina} \nsua nota do 1º bimestre foi {nota1} \nsua nota do 2º bimestre foi {nota2} \nsua nota do 3º bimestre foi {nota3} \nsua nota do 4º bimestre foi {nota4}")
