0ddf9f693d43927f4ea33daeea578d164fd0f961
geovannadarosa <geovannadarosava@gmail.com>
2025-12-07 19:43:32 -0400

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,6 @@
+num1 = float(input('digite um numero:')) 
+num2 = float(input('digite um numero:')) 
+num3 = float(input('digite um numero:')) 
+
+max = max(num1, num2, num3)
+print(f"o seu numero maior é {max}")
