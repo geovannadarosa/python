80df10dec6f9e644da382ebe92dc721c94dbac71
geovannadarosa <geovannadarosava@gmail.com>
2026-02-10 20:29:22 -0400

    Add files via upload

---
+++ atividadelista14.py
@@ -0,0 +1,67 @@
+#1
+
+# import math
+
+# numero = float(input("Digite um número: "))
+# raiz = math.sqrt(numero)
+
+# print("Raiz quadrada:", raiz)
+
+#2
+
+# from math import pow
+
+# n1 = float(input("Digite a base: "))
+# n2 = float(input("Digite o expoente: "))
+
+# resultado = pow(n1, n2)
+# print("Resultado:", resultado)
+
+#3
+
+# import math as m
+
+# numero = int(input("Digite um número negativo: "))
+# valor = m.fabs(numero)
+
+# print("Valor absoluto:", valor)
+
+#4
+
+# import random
+
+# numero = random.randint(1, 50)
+# print("Número sorteado:", numero)
+
+
+#5
+
+# import math
+# import random
+
+# num = random.randint(1, 100)
+# raiz = math.sqrt(num)
+
+# print("Número:", num)
+# print("Raiz:", raiz)
+
+#6
+
+
+# import math
+# import random
+
+# num = random.randint(1, 100)
+# raiz = math.sqrt(num)
+
+# print("Número:", num)
+# print("Raiz:", raiz)
+
+#7
+#oq esta na outra parte
+'def boas_vindas(): '
+'print("Seja bem-vindo ao sistema!")'
+
+import mensagens
+
+mensagens.boas_vindas()
